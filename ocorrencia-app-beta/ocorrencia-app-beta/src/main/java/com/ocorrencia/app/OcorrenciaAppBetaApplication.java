87a399563590928b141189d9f8e8c25a00787af9
SosthenesMS <sosthenesms@yahoo.com.br>
2023-03-20 20:55:34 -0300

    Commit inicial

---
+++ ocorrencia-app-beta/ocorrencia-app-beta/src/main/java/com/ocorrencia/app/OcorrenciaAppBetaApplication.java
@@ -0,0 +1,13 @@
+package com.ocorrencia.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcorrenciaAppBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcorrenciaAppBetaApplication.class, args);
+	}
+
+}
